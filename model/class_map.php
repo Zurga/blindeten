b99cbf6bde895c5193ce9374d1c74c354fe0d0ff
Jim <jim.lemmers@gmail.com>
2015-01-09 21:32:03 +0100

    added map class

--- model/class_map.php
+++ model/class_map.php
@@ -15,7 +15,7 @@ class Map{
 			' WHERE tables.user2 is NULL';
 		
 		$result = $db->query($query);
-		var_dump($resulit);
+		var_dump($result);
 		
 		if ($result->num_row > 0) {
 			$row = $result->fetch_assoc();
