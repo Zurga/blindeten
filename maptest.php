7c8467672b09b457b33f243e94756815bfd8a0a3
Jim <jim.lemmers@gmail.com>
2015-01-09 23:45:58 +0100

    javascript creation test

--- maptest.php
+++ maptest.php
@@ -5,3 +5,17 @@ $map = new Map;
 $map->get_geo_info();
 var_dump($map);
 ?>
+<html>
+<head>
+<script src='http://openlayers.org/api/OpenLayers.js'></script>
+</head>
+<body>
+<div id='Map'></div>
+<script>
+<?php
+foreach($map as $marker){
+	echo "var div= '<div>'". $marker['name'] . $marker['url'] . '</div>';
+}
+</script>
+</body>
+</html>
