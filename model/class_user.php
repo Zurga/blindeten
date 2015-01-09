4bc9698ac6b1204ee4231b85f6b85a9c91c041b9
Rens <rens.mester@hotmail.com>
2015-01-09 15:46:44 +0100

    test php

--- model/class_user.php
+++ model/class_user.php
@@ -58,9 +58,10 @@ class User {
 
 $test = new User;
 $test->get_user('rens.mester@hotmail.com');
+var_dump($test);
 $attr = array('name'=>'Rens Mester','sex'=>'0','birthdate'=>'1995-09-30','city'=>'Hoorn');
 
-var_dump($test->get_user('rens.mester@hotmail.com'));
+var_dump($test);
 echo '<br>';
 echo '<br>';
 $test->change_attr($attr);
