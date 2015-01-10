7d7f29bd0eddb95646d2d328b35cafc90ca9446d
Jim <jim.lemmers@gmail.com>
2015-01-11 00:14:49 +0100

    testing

--- maptest.php
+++ maptest.php
@@ -35,7 +35,7 @@ for(i=0;i<json.length;i++){
 		);
 	marker = new OpenLayers.Marker(lonlat);
 	marker.events.register("click", marker, function(e){
-		var popup = new OpenLayers.Popup.FramedCloud("text", clone(this.lonlat]), popup_size, 
+		var popup = new OpenLayers.Popup.FramedCloud("text", clone(this.lonlat), popup_size, 
 			"<div style='width: 26px; height:20px;'>"+ marker.lonlat+"Text</div>", null, true);
 		popup.updatePosition();
 		console.log(this.lonlat);
