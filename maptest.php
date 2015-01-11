f678ed5ce754e07a97327361da1de80ac564b6af
Jim <jim.lemmers@gmail.com>
2015-01-11 01:13:39 +0100

    change map layout

--- maptest.php
+++ maptest.php
@@ -16,7 +16,7 @@ $map->get_geo_info();
 ol  = OpenLayers;
 map = new OpenLayers.Map({target: 'map',
 	layers: [
-	new ol.layer.Tile({source: new ol.source.XYZ({ 
+	new ol.Layer.Tile({source: new ol.source.XYZ({ 
 	url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
 		attributions: [new ol.Attribution({ 
 		html: ['&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'] })]
