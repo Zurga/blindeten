bb19aea3ee0f8f7da2998248c10044c33998775d
Jim <jim.lemmers@gmail.com>
2015-01-11 16:00:08 +0100

    removed openlayers, now using google maps

--- maptest.php
+++ maptest.php
@@ -6,53 +6,38 @@ $map->get_geo_info();
 ?>
 <html>
 <head>
-<script src='js/ol.js'></script>
+ <script src="http://maps.google.com/maps/api/js?sensor=false" 
+          type="text/javascript"></script>
 </head>
 <body>
 <div id='Map'></div>
 <script>
-map = new ol.Map('Map');
-map.addLayer(new ol.Layer.OSM());
+map = new google.maps.Map(document.getElementById('map'),{
+	zoom: 3,
+	center: new google.maps.LatLng(52, 4)
+});
 //create json from the model
 var json = [ <?php foreach($map->markers as $marker){echo json_encode($marker) . ',';}?>]
-var markers = new ol.Layer.Markers("Restaurants");
-map.addLayer(markers);
-var popup_size = new ol.Size(200, 200);
 
 for(i=0;i<json.length;i++){
 	//create lonlat for each restaurant
-	var lonlat = new ol.LonLat(json[i]['lon'], json[i]['lat'])
-		.transform( new ol.Projection("EPSG:4326"),
-			map.getProjectionObject()
-		);
-	marker = new ol.Marker(lonlat);
-	marker.id = json[i].id;
-	marker.name = json[i].name
+	var latlon = new google.maps.LatLng(json[i].lat, json[i].lon);
+	marker = new google.maps.Marker({
+		position: latlon,
+		map: map
+		});
+	infowindow = new google.maps.InfoWindow();
+
 	//todo create icon for the marker
 	//creating the popup for each restaurant
-	marker.events.register("click", marker, function(e){
-		popups = document.getElementsByClassName('olPopup');
-		//hide all other popups
-		for(i = 0; i<popups.length; i++){
-			popups[i].style.display = 'none';
+	google.maps.event.addListener(marker, 'click', (function(marker, i){
+		return function(){
+			infowindow.setContent(json[i].name);
+			infowindow.open(map,marker);
 		}
-		//check if the popup exists in the html
-		pop = document.getElementById(this.id);
-		if (pop != null){
-			//show the popup
-			pop.style.display = '';
-		}
-		else{	//create new popup
-			var popup = new ol.Popup.FramedCloud(this.id, 
-				this.lonlat, popup_size, 
-				"<div style='width: 26px; height:20px;'>" +  
-				"Text</div>", null, true);
-			map.addPopup(popup)
-		}
-	});
-	markers.addMarker(marker);
+	})(marker, i));
 }
-map.setCenter(lonlat, 14);
+//map.setCenter(lonlat, 14);
 
 </script>
 </body>
