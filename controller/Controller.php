1a4a8663300d6f5e291372b620b9b90b68379ea4
Jim <jim.lemmers@gmail.com>
2015-01-12 12:10:34 +0100

    includes fixed

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,17 +1,17 @@
 <?php
 
-include '/model/Model.php';
+include_once 'model/Model.php';
 
 class Controller {
 	public $model;
 
 	public function __construct(){
-		$this->model = new Model();
+		$this->model = new Model;
 	}
 
 	public function invoke(){
 		$restaurants = $this->model->get_restaurants();
-		include 'view/map.php';
+		include '/view/map.php';
 	}
 }
 ?>
