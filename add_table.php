ee97597b9a7e9dacd257646e39d84dea05ec2260
Rens <rens.mester@hotmail.com>
2015-01-13 13:20:56 +0100

    bugfix

--- add_table.php
+++ add_table.php
@@ -6,7 +6,7 @@ include_once "model/Restaurant.php";
 $user = new User;
 $restaurant = new Restaurant;
 $restaurant->id=2;
-$attr = array('rest_id'=>'9');
+$attr = array('rest_id'=>'2');
 $user->add_table($restaurant);
 
 ?>
