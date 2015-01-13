c63e5a7121039692dd6766dd0443e018e46aaa4f
Jim <jim.lemmers@gmail.com>
2015-01-13 12:12:38 +0100

    bugfix model

--- UserLogin.php
+++ UserLogin.php
@@ -3,9 +3,9 @@ include_once "model/Model.php";
 
 $model = new Model;
 $attr = array('name'=>'Rens Mosterd',
-'email'=>'rens.mosterd@hotmail.com',
+'email'=>'rens.mosterd2@hotmail.com',
 'sex'=>'0',
-'birthdate'=>'1995-09-31',
+'birthdate'=>'1995-09-30',
 'city'=>'Hoorn',
 'password'=>'blabla');
 $user = $model->add_account($attr);
