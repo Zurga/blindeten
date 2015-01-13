19135c99fe7e1250a414d4ebdc0e16a29975946c
Rens <rens.mester@hotmail.com>
2015-01-13 11:24:25 +0100

    test files

---
+++ Test/UserLogin.php
@@ -0,0 +1,17 @@
+<?php
+include "model/Model.php";
+
+$model = new Model;
+$attr = array('name'=>'Rens Mester',
+'email'=>'rens.mester@hotmail.com',
+'sex'=>'0',
+'birthdate'=>'1995-09-31',
+'city'=>'Hoorn',
+'password'=>'blabla');
+$user = $model->add_account($attr);
+
+
+
+
+
+?>
