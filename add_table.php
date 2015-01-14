530274d7c223981fc4c47ec995b10ebbce4fd9e6
Rens <rens.mester@hotmail.com>
2015-01-14 17:04:44 +0100

    bugfix

--- add_table.php
+++ add_table.php
@@ -1,10 +1,8 @@
 <?php
-include_once "model/Model.php";
-include_once "model/User.php";
-include_once "model/Restaurant.php";
+$root = $_SERVER['DOCUMENT_ROOT'];
+include $root . '/model/User.php';
 
-$user = new User;
-$user->permission='Admin';
+$user = new User(2);
 $restaurant = new Restaurant;
 $restaurant->id=2;
 $attr = array('rest_id'=>'2');
