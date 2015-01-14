149cb1438ea1eacdad9cc6bd8de0bdb540db64e1
Rens <rens.mester@hotmail.com>
2015-01-14 15:19:31 +0100

    bugfix

--- test/delete_account.php
+++ test/delete_account.php
@@ -4,7 +4,9 @@ include $root . '/model/User.php';
 
 $user = new User(2);
 echo '<br>';
-$user_id=15;
+$user_id=26;
+$user->delete_account($user_id);
+$user_id=32;
 $user->delete_account($user_id);
 
 ?>
