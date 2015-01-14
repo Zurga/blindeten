29616b935167c2141c79673fdf0793f9200ac1dd
Rens <rens.mester@hotmail.com>
2015-01-14 15:16:54 +0100

    bugfix

--- test/delete_account.php
+++ test/delete_account.php
@@ -2,7 +2,7 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 include $root . '/model/User.php';
 
-$user = new User(15);
+$user = new User(2);
 echo '<br>';
 $user_id=15;
 var_dump($user->delete_account($user_id));
