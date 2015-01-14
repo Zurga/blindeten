a24b8a0883578533c46cb5ce9d629f6c4fff82d2
Jim <jim.lemmers@gmail.com>
2015-01-14 11:06:33 +0100

    bugfix

--- model/Model.php
+++ model/Model.php
@@ -88,8 +88,8 @@ class Model{
 			$query = "SELECT id FROM bookings" .
 				" WHERE table_id = " . $table_id . 
 				" AND time = ". $time;
-			$exists = get_rows($this->db->($query)); 
-			if ($exists){
+
+			if ($exists = get_rows($this->db->query($query))){
 				//it exists
 				$user1 = $exists['user1'];
 				$bookQ = "UPDATE bookings SET user2 = " . $user->id.
