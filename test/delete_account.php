f662b685cae09dc68f02c8e8d3cb47f864c95f92
Rens <rens.mester@hotmail.com>
2015-01-14 14:34:39 +0100

    bugfix del_account

--- test/delete_account.php
+++ test/delete_account.php
@@ -2,8 +2,7 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 include $root . '/model/User.php';
 
-$user = new User;
-$user->permission='Admin';
+$user = new User(2);
 $user_id=30;
 var_dump($user->delete_account($user_id));
 
