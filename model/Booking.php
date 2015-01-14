45605126b84f7bfc80f8a0417b9cf90abbe5d92e
Rens <rens.mester@hotmail.com>
2015-01-14 17:45:27 +0100

    bugfix

--- model/Booking.php
+++ model/Booking.php
@@ -6,4 +6,25 @@ class Booking{
 	public $user1;
 	public $user2;
 	public $time;
+	
+	public function __construct($id){
+		global $db;
+		$this->db = $db;
+
+		$query = "SELECT id, table_id, user1 ," .
+			" user2, time".
+			" FROM bookings" .
+			" WHERE id = " . $id;
+		
+		if($row = get_rows($this->db->query($query))){
+			//assign values to user based on mySQL columns
+			foreach($row as $key=>$val){
+				$this->$key = $val;
+			}
+			return $this;
+		}
+		else{
+			return false; 
+		}
+	}
 }
