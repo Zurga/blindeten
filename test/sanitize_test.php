c37ff0a4256fd9bfe3ed02f764e903b6bc999f03
Rens <rens.mester@hotmail.com>
2015-01-14 13:25:46 +0100

    bugfix

--- test/sanitize_test.php
+++ test/sanitize_test.php
@@ -8,7 +8,10 @@ $test3 = array('query'=>'OR "1"');
 $test4 = 'OR "1"';
 
 var_dump(sanitize($test1));
+echo '<br>';
 var_dump(sanitize($test2));
+echo '<br>';
 var_dump(sanitize($test3));
+echo '<br>';
 var_dump(sanitize($test4));
 ?>
