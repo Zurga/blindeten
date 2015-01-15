0aaa1dddee2262037691d1639322124f6730496f
Jim <jim.lemmers@gmail.com>
2015-01-15 10:22:56 +0100

    added auth file

--- model/Restaurant.php
+++ model/Restaurant.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once 'dblogin.php';
+include_once 'dbFunctions.php';
+
 class Restaurant{
 	public $id;
 	public $name;
@@ -12,7 +15,15 @@ class Restaurant{
 	public $tables;
 
 	function __construct($id){
+		global $db;
+		$this->db = $db;
+
 		$query = "SELECT restaurant.name, restaurant.lat, restaurant.lon," .
 			" restaurant.street, restaurant.city, restaurant.url";
+
+		if($row = get_rows($this->db->query($query))){
+		}
+
 	}
 }
+?>
