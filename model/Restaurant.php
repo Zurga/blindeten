e666af89af1ddafc73097cb13dc189b17aa36a87
Jim <jim.lemmers@gmail.com>
2015-01-15 09:10:43 +0100

    just to be sure

--- model/Restaurant.php
+++ model/Restaurant.php
@@ -5,6 +5,14 @@ class Restaurant{
 	public $name;
 	public $lat;
 	public $lon;
+	public $street;
+	public $number;
+	public $city;
 	public $url;
 	public $tables;
+
+	function __construct($id){
+		$query = "SELECT restaurant.name, restaurant.lat, restaurant.lon," .
+			" restaurant.street, restaurant.city, restaurant.url";
+	}
 }
