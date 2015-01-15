5531d4e05bd195017d06c9d233b48afb670780b2
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-15 13:16:22 +0100

    if restaurant

--- html/show.php
+++ html/show.php
@@ -19,10 +19,10 @@ echo '
 <div class="content">
 	<div class="maincontent">
 		<h1>Mijn Restaurant</h1>
-		<p>Naam:'. $restaurant->name; .'</p>
-		<p>URL:'.$restaurant->url; .'</p>
-		<p>Aantal tafels:'. $restaurant->tables; .'</p>
-		<p>Adres:'.$restaurant->street;  $restaurant->number; $restaurant->zipcode; restaurant->city; .'</p>
+		<p>Naam:'. $restaurant->name .'</p>
+		<p>URL:'.$restaurant->url .'</p>
+		<p>Aantal tafels:'. $restaurant->tables .'</p>
+		<p>Adres:'.$restaurant->street $restaurant->number $restaurant->zipcode restaurant->city .'</p>
 		<p><a href="/account/edit.php" title="Gegevens wijzigen">Gegevens wijzigen</a>
 		<br>		
 		<a href="wachtwoordveranderen.php" title="Wachtwoord veranderen">Wachtwoord veranderen</a>
