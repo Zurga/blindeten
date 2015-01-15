8e75380fb286a5783a63904b4f0c7ce2e2f7d7f1
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-15 15:43:06 +0100

    edit booking

--- html/uitloggen.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php';?>
-
-<div id="uitloggen">
-	<h4>Welkom user_name! <a href="mijnaccount.php">Mijn Account</a> <a href="header.php">Uitloggen</a></h4>
-</div>
-
-<?php include 'footer.php';?>
