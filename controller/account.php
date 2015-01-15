11df05922f4a5a9ab0799f67cf6772bc3b0aed6f
Jim <jim.lemmers@gmail.com>
2015-01-15 13:18:26 +0100

    bugfiq

--- controller/account.php
+++ controller/account.php
@@ -2,6 +2,12 @@
 
 if($request == '/account/'){
 	$user = new User(2);
+	$title = 'Account';
+	var_dump($user);
+	if($user->owner != 0){
+		$restaurant = new Restaurant($user->owner);
+	}
+
 	include $root . '/html/show.php';
 }
 ?>
