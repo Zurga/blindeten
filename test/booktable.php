edaf6b21709c0addc8316143c37d8f6d54a224f2
Jim <jim.lemmers@gmail.com>
2015-01-15 16:30:42 +0100

    booktable

--- test/booktable.php
+++ test/booktable.php
@@ -6,9 +6,8 @@ include_once $root ."model/User.php";
 include_once $root ."model/Restaurant.php";
 
 $user = new User(4);
-$restaurant = new Restaurant;
+$restaurant = new Restaurant(2);
 $model = new Model;
-$restaurant->id=2;
 $restaurant->tables=array(4,5);
 $table_id=4;
 var_dump($model->book_table($user,$restaurant,$table_id, '2015-02-04 18:00:00'));
