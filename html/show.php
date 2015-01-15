c3fed49413065bc2f765ab1c6210c7276b4a88e1
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-15 13:30:14 +0100

    adres mijn restaurant

--- html/show.php
+++ html/show.php
@@ -22,7 +22,7 @@ echo '
 		<p>Naam:'. $restaurant->name .'</p>
 		<p>URL:'.$restaurant->url .'</p>
 		<p>Aantal tafels:'. $restaurant->tables .'</p>
-		<p>Adres:'. [$restaurant->street], [$restaurant->number] , [$restaurant->zipcode] , [restaurant->city] .'</p>	
+		<p>Adres:'. $restaurant['street'], $restaurant['number'] , $restaurant['zipcode'], $restaurant['city'] .'</p>	
 		<p><a href="/account/edit.php" title="Gegevens wijzigen">Gegevens wijzigen</a>
 		<br>		
 		<a href="wachtwoordveranderen.php" title="Wachtwoord veranderen">Wachtwoord veranderen</a>
