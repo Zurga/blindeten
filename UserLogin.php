9b74810ac98680359043f6a66f52b3bf5e7979b4
Jim <jim.lemmers@gmail.com>
2015-01-16 14:05:51 +0100

    login test

--- UserLogin.php
+++ UserLogin.php
@@ -1,17 +1,15 @@
 <?php
 include_once "model/Model.php";
+include_once "model/Auth.php";
 
 $model = new Model;
+$auth = new Auth;
 $attr = array('name'=>'Jaap Testpersoon',
-'email'=>'JT2@hotmail.com',
+'email'=>'JT3@hotmail.com',
 'sex'=>'0',
 'birthdate'=>'1995-08-22',
 'city'=>'Urk',
 'password'=>'blabla');
 $user = $model->add_account($attr);
-
-
-
-
-
+echo $auth->Login($user->email, $user->password);
 ?>
--- controller/account.php
+++ controller/account.php
@@ -2,8 +2,9 @@
 var_dump($_POST);
 echo '<br>';
 var_dump($_SESSION);
+include_once $root . '/model/Auth.php';
+
 if($request == '/account/'){
-	$user = new User(4);
 	$title = 'Account';
 	
 	if($user->owner != 0){
@@ -16,8 +17,9 @@ if($request == '/account/login.php' & empty($_POST)){
 	include $root . '/html/login.php';
 }
 else{
-	include_once $root . '/model/Auth.php';
 	$login = new Login;
+	var_dump($_POST);
+	echo '<br/>';
 	var_dump($login->login($_POST['email'], $_POST['password']));
 }
 ?>
