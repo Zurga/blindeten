f1768a8577d11faf42d08e2d2a125a9d25eea8f8
Jim <jim.lemmers@gmail.com>
2015-01-16 14:09:23 +0100

    login test

--- UserLogin.php
+++ UserLogin.php
@@ -5,11 +5,12 @@ include_once "model/Auth.php";
 $model = new Model;
 $auth = new Login;
 $attr = array('name'=>'Jaap Testpersoon',
-'email'=>'JT3@hotmail.com',
+'email'=>'JT4@hotmail.com',
 'sex'=>'0',
 'birthdate'=>'1995-08-22',
 'city'=>'Urk',
 'password'=>'blabla');
-$user = $model->add_account($attr);
+$user = new User($model->add_account($attr));
+
 echo $auth->Login($user->email, $user->password);
 ?>
