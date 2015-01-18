33d38e35fc71af4c867285ca608e663331ddaa83
Jim <jim.lemmers@gmail.com>
2015-01-18 13:50:17 +0100

    redirect to index after login

--- controller/account.php
+++ controller/account.php
@@ -21,6 +21,6 @@ else{
 //	var_dump($_POST);
 //	echo '<br/>';
 //	var_dump($login->login($_POST['email'], $_POST['password']));
-	header("Location : http://ik35.webdb.fnwi.uva.nl");
+	header("Location: http://ik35.webdb.fnwi.uva.nl");
 }
 ?>
