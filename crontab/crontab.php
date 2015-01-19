0aa67a6e9e13df31d91452f13f64531ead33d2c5
Rens <rens.mester@hotmail.com>
2015-01-19 16:51:04 +0100

    add

---
+++ crontab/crontab.php
@@ -0,0 +1,26 @@
+<?php
+public function add_history(){	
+	//select correct bookings, move to history, delete booking
+	$query = "SELECT restaurant_id,time,user1,user2 FROM bookings WHERE time".
+		" > ". time();
+		
+	if ($bookings = get_rows($this->db->query($query))) {
+		foreach ($bookings as $booking) {
+			var_dump($booking);
+			$hist_query = "INSERT INTO history (user_id,".
+				"restaurant_id,bookings_time)".
+				" VALUES (". $booking['user1'] .",". 
+				$booking['restaurant_id'] .",'".
+				$booking['time']. "'),(". 
+				$booking['user2'] .",". 
+				$booking['restaurant_id'] .",'". 
+				$booking['time']. "')";
+			$this->db->query($hist_query);
+			return true;	
+		}
+	}
+	else {
+		return false;
+	}
+}
+?>
