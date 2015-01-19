5ab6b0485fff51b20091a9c248252f8d03db886b
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-19 13:18:36 +0100

    header fixen

--- html/header.php
+++ html/header.php
@@ -12,14 +12,9 @@
 
 <div id="menu">
     <ul>
-<<<<<<< HEAD
-        <li><a href="info.php" title="Wat is BlinDeten?">Wat is BlinDeten?</a></li>  
-        <li><a href="registreer.php" title="Registreer">Registreer</a></li>  
-		<li><a href="login.php" title="Log in">Log in</a></li> 
-=======
         <li><a href="/text/info.php" title="Wat is BlinDeten?">Wat is BlinDeten?</a></li>  
         <li><a href="/account/register.php" title="Registreer">Registreer</a></li>  
 		<li><a href="/account/login.php" title="Log in">Log in</a></li> 
->>>>>>> 9a283729a9b576def1e2c979359f520b4a46d944
+
 <br>
 </div>
