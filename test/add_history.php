6f95b0deb9238b459a31375ae10d440fffe6dfc6
Rens <rens.mester@hotmail.com>
2015-01-19 10:46:42 +0100

    bugfix

--- test/add_history.php
+++ test/add_history.php
@@ -6,6 +6,6 @@ include $root . '/model/Model.php';
 $model = new Model;
 $user = new User(2);
 $booking_id = 1;
-var_dump(add_history($user,$booking_id));
+var_dump($model->add_history($user,$booking_id));
 
 ?>
