3ac36255a5bcdb5461d752ebbb5b5dad466cc7ca
Jim <jim.lemmers@gmail.com>
2015-01-19 10:56:01 +0100

    login test

--- UserLogin.php
+++ UserLogin.php
@@ -5,7 +5,7 @@ session_start();
 $model = new Model;
 $auth = new Login;
 $attr = array('name'=>'Jaap Testpersoon',
-'email'=>'JT5@hotmail.com',
+'email'=>'JT6@hotmail.com',
 'sex'=>'0',
 'birthdate'=>'1995-08-22',
 'city'=>'Urk',
