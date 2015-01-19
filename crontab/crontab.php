e0eee4a89d3a4abd37458bebaca5f300e32a61f2
Rens <rens.mester@hotmail.com>
2015-01-19 17:04:28 +0100

    bugfix

--- crontab/crontab.php
+++ crontab/crontab.php
@@ -1,10 +1,14 @@
 <?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+include_once 'dblogin.php';
+
+
 public function add_history(){	
 	//select correct bookings, move to history, delete booking
 	$query = "SELECT restaurant_id,time,user1,user2 FROM bookings WHERE time".
 		" > ". time();
 		
-	if ($bookings = get_rows($this->db->query($query))) {
+	if ($bookings = get_rows($db->query($query))) {
 		foreach ($bookings as $booking) {
 			var_dump($booking);
 			$hist_query = "INSERT INTO history (user_id,".
