9d8997896bbf49319d02870b175dab9a5ea8b6da
Jim <jim.lemmers@gmail.com>
2015-01-19 16:12:52 +0100

    dbfunctions var_dump

--- model/dbFunctions.php
+++ model/dbFunctions.php
@@ -5,7 +5,7 @@ function get_rows($result){
 	if ($result->num_rows == 1){
 			$row = $result->fetch_assoc();
 			var_dump(array($row));
-			return array($row);
+			return $row;
 		}
 	else if($result->num_rows > 1){
 		$rows = array();
