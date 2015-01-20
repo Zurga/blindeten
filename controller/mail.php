0ae0b9b4a7cbdbfb526e02e273b987bf23bf29d6
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-20 12:49:31 +0100

    mail

--- controller/mail.php
+++ controller/mail.php
@@ -1,18 +1,24 @@
 <?php
 $root = $_SERVER['DOCUMENT_ROOT'];
 include_once $root . '/model/User.php';
+include_once "dblogin.php";
+include_once "dbFunctions";
 
 ini_set('display_errors',1);
 
-function test_mail($user) {
-	$to = 'rens.mester@gmail.com'; //$user->email;
-	$subject = 'mail.php';
-	$message = 'HALLO DIT IS EEN TEST';
+function test_mail($user,$mail_id) {
+	global $db;
+	$query = "SELECT * FROM mail WHERE id=". $mail_id;
+	
+	$mail_info = get_rows($db->query($query));
+	$to = "jim.lemmers@gmail.com"//$user->email;
+	$subject = $mail_info['subject'];
+	$message = $mail_info['message'];
 	$headers = 'From: Jim.lemmers@gmail.com';
 	
 	mail($to,$subject,$message,$headers);
 }
 
 $user = new User(2);
-test_mail($user);
+test_mail($user, 1);
 ?>
