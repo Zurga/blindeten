b5fe611edf90f256d83182baf229791e78099161
Jim <jim.lemmers@gmail.com>
2015-01-20 17:43:18 +0100

    login fix

--- controller/account.php
+++ controller/account.php
@@ -1,13 +1,15 @@
 <?php
 //var_dump($_POST);
 //echo '<br>';
+include_once $root . '/model/Auth.php';
+$auth = new Login;
+$logged_in = $auth->check_login();
+var_dump($logged_in);
 
-if($request == '/account/show.php'){ 
-	if($logged_in){
+if($request == '/account/show.php'){
+	if($_SESSION['logged_in']){
 		$title = 'Account';
-		var_dump($_SESSION['id']);
 		$user = new User($_SESSION['id']);
-		var_dump($user);
 		//$age
 		if($user->owner != 0){
 			$restaurant = new Restaurant($user->owner);
@@ -26,17 +28,15 @@ if ($request == '/account/logout') {
 	header("Location: http://ik35.webdb.fnwi.uva.nl");
 }	
 
-if($request == '/account/login.php' & $logged_in == false){
+if($request == '/account/login.php' & $logged_in == false & empty($_POST)){
 	include $root . '/html/login.php';
 }
-
-if($request == '/account/set_login'){
+else{
 	$login = new Login;
 	if($logged_in =  $login->login($_POST['email'], $_POST['password'])){
 		header("Location: http://ik35.webdb.fnwi.uva.nl");
 	}
 	else{
-		echo 'niet gelukt';
 		include $root . '/html/login.php';
 	}
 }
