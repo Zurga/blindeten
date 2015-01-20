e838dd652ccfb958f8c240fea306a851299bc267
Jim <jim.lemmers@gmail.com>
2015-01-20 15:34:35 +0100

    import functions.js

--- html/index.php
+++ html/index.php
@@ -71,6 +71,43 @@
     
         			alert("a date was just selected and the date is : " + obj.day + "/" + obj.month + "/" + obj.year);
     				});} }
+
+// Get the HTTP Object
+function get_http_Object(){
+	if (window.ActiveXObject) 
+		return new ActiveXObject("Microsoft.XMLHTTP");
+        else if (window.XMLHttpRequest) 
+	     	return new XMLHttpRequest();
+        else {
+              alert("Your browser does not support AJAX.");/                                 return null;
+        }
+}
+function get_output(which, input){
+	http_object = get_http_object();
+	if (http_object != null){
+		http_object.open('GET', "ajax/"+ which + "?input=" + input,
+				true);
+		http_object.send(null);
+		http_object.onreadystatechange = set_output(id);
+	}
+}
+
+function set_output(id){
+	if(http_object.ready_state == 4){
+		document.getElementById(id).innerHtml = http_object.responseText;
+	}
+}	
+
+function showtext(id){
+   			
+if(document.getElementById(id).style.display == 'none'){
+    document.getElementById(id).style.display = 'block';
+   	}
+   		
+else{
+      	document.getElementById(id).style.display = 'none';      
+   		}
+		}
 		</script>
 
 <script src="http://maps.google.com/maps/api/js?sensor=false" 
