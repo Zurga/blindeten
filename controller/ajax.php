4a39f8e1296bf79f7f13f5b45dec176426408824
Jim <jim.lemmers@gmail.com>
2015-01-20 16:41:38 +0100

    import functions.js

---
+++ controller/ajax.php
@@ -0,0 +1,7 @@
+<?php
+
+if($request == '/ajax/calendar'){
+	echo 'het werkt, het restaurant id = ' . $_GET['input'];
+	//$bookings = $this->model->get_bookings($_GET['input']);
+	//include $root . '/html/calendar.php';
+}
