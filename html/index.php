5fac3ce3d2d245b73c18cfc576407fb8dd3a40cc
Fleurkoentjes <fleurkoentjes@gmail.com>
2015-01-20 14:45:42 +0100

    calender

--- html/index.php
+++ html/index.php
@@ -62,7 +62,7 @@
         			useMode:1,
         			isStripped:true,
         			target: id,
-	  				cellColorScheme:"deepblue"
+	  				cellColorScheme:"lightgreen"
     				}); 
     				calendar.setOnSelectedDelegate(function(){
         			var obj = calendar.getSelectedDay();
