9952b848b608fdcd2afa2506c69f9197871964ca
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-20 13:39:41 +0100

    bugfix

--- controller/mail.php
+++ controller/mail.php
@@ -13,7 +13,7 @@ function test_mail($user,$mail_id) {
 	$mail_info = get_rows($db->query($query));
 	$to = "rens.mester@gmail.com";//$user->email
 	$subject = $mail_info['subject'];
-	$message = "Welkom %user% bij BlinDeten, \r\nWaar wacht je nog op? Maak je eerste reservering! \r\n \r\nMet vriendelijke groeten, \r\n \r\nHet BlinDeten team!";//$mail_info['message'];
+	$message = "Welkom %user% bij BlinDeten, \r\n \r\nWaar wacht je nog op? Maak je eerste reservering! \r\n \r\nMet vriendelijke groeten, \r\n \r\nHet BlinDeten team!";//$mail_info['message'];
 	$headers = 'From: Jim.lemmers@gmail.com';
 	
 	mail($to,$subject,$message,$headers);
