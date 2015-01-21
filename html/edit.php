2649ac8924dbf33df067e995d1589067a2569508
Jim <jim.lemmers@gmail.com>
2015-01-21 13:49:14 +0100

    edit.php added echo

--- html/edit.php
+++ html/edit.php
@@ -26,30 +26,29 @@
 			<li><input type="submit" id="submit" value="Account verwijderen"></p></li>
 		</fieldset>	
 		</form>
-		
+<?php if(isset($restaurant)){ ?>	
 		<form>	
 		<fieldset id="inputs">
 			<li><p>Naam Restaurant:</p><br><br>
-			<p><input id="name" name="input[name]" type="text" placeholder="<?php $restaurant->name ?>" ></p></li>
+			<p><input id="name" name="input[name]" type="text" placeholder="<?php echo $restaurant->name; ?>" ></p></li>
 			<br>
 			<li><p>Aantal tafels:</p><br><br>
-			<p><input id="tables" name="input[tables]" type="number" placeholder="<?php $restaurant->tables ?>" ></p></li>
+			<p><input id="tables" name="input[tables]" type="number" placeholder="<?php echo $restaurant->tables; ?>" ></p></li>
 			<br>
 			<li><p>Website URL:</p><br><br>
-			<p><input id="url" name="input[url]" type="text" placeholder="<?php $restaurant-> url ?>" ></p></li>
+			<p><input id="url" name="input[url]" type="text" placeholder="<?php echo $restaurant->url; ?>" ></p></li>
 			<br>
 			<p>Straat:</p>
-			 <p><input id="street" name="input[street]" type="text" placeholder="<?php $restaurant->street ?>" ></p></li>
+			 <p><input id="street" name="input[street]" type="text" placeholder="<?php echo $restaurant->street; ?>" ></p></li>
 			
 			<p>Huisnummer:</p>
-			 <p><input id="number" name="input[number]" type="text" size="4" placeholder="<?php $restaurant->number ?>" ></p></li>
+			 <p><input id="number" name="input[number]" type="text" size="4" placeholder="<?php echo $restaurant->number; ?>" ></p></li>
 			<br>
 			<br>
 			<p>Postcode:</p>
-			 <p><input id="zipcode" name="input[zipcode]" type="text" size="8" maxlength="6" placeholder="<?php $restaurant->zipcode ?>" ></p></li>
-			
+			 <p><input id="zipcode" name="input[zipcode]" type="text" size="8" maxlength="6" placeholder="<?php echo $restaurant->zipcode ?>" ></p></li>			
 			<p>Stad:</p>
-			 <p><input id="city" name="input[city]" type="text" placeholder="<?php $restaurant->city ?>" ></p></li>
+			 <p><input id="city" name="input[city]" type="text" placeholder="<?php echo $restaurant->city; ?>" ></p></li>
 			<br>
 			<br>
 			<br>
@@ -60,5 +59,6 @@
 		</form>
 	</div>
 </div>
+<?php } ?>
 
 <?php include 'footer.php';?>
