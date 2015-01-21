0a6db9a1d81aec35c29ff124bf92b68a3825c1fb
Jim <jim.lemmers@gmail.com>
2015-01-21 13:22:41 +0100

    login redirect

--- model/User.php
+++ model/User.php
@@ -56,7 +56,7 @@ class User {
 
 		if($db->query($query)){
 			$rest_id = $db->insert_id;
-			$qeury = 
+			$qeury;
 			//TO DO add tables to restaurants
 			//foreach($attr['tables'] as table){
 			//	$this->add_table();
