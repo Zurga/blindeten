f144e2bef887cc9aeefbea245418e141fa53e158
Rens <rens.mester@hotmail.com>
2015-01-21 12:42:03 +0100

    bugfix get_booking

--- test/add_history.php
+++ test/add_history.php
@@ -3,6 +3,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 include $root . '/model/User.php';
 include $root . '/model/Model.php';
 
+
 $model = new Model;
 $user = new User(4);
 //$booking_id = 1;
--- test/get_booking.php
+++ test/get_booking.php
@@ -2,6 +2,7 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 include $root . '/model/User.php';
 include $root . '/model/Restaurant.php';
+include $root . '/model/Booking.php';
 
 $user = new User(2);
 $rest = new Restaurant(6);
