ac1a8aabbb2f20abc2cdebd378e8dd1adfb032a3
Jim <jim.lemmers@gmail.com>
2015-01-21 11:14:13 +0100

    get_rows test

--- test/booktable.php
+++ test/booktable.php
@@ -4,9 +4,32 @@ $root = $_SERVER['DOCUMENT_ROOT'] . '/';
 include_once $root ."model/Model.php";
 include_once $root ."model/User.php";
 include_once $root ."model/Restaurant.php";
+include_once $root ."model/dblogin.php";
+global $db;
 
+$query = "SELECT * from user_perm WHERE 1";
+$result =  $db->query($query);
+var_dump($result);
+echo '<br/>';
+
+if ($result->num_rows == 1){
+		$row = $result->fetch_assoc();
+		var_dump($row);
+	}
+else if($result->num_rows > 1){
+	$rows = array();
+	while ($row = $result->fetch_assoc()){
+		$rows[] = $row;
+	}
+	var_dump($rows);
+}
+
+else{
+	return false;
+}
+/*
 $user = new User(2);
-var_dump($user);
+vddar_dump($user);
 echo '<br/>';
 $restaurant = new Restaurant(2);
 var_dump($restaurant);
@@ -14,5 +37,5 @@ $model = new Model;
 $restaurant->tables=array(4,5);
 //$table_id=4;
 //var_dump($model->book_table($user,$restaurant,$table_id, '2015-02-04 18:00:00'));
-
+ */
 ?>
