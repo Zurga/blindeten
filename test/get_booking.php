08adfbbf40916ffd19a9538fdfa30f1f612e7461
Rens <rens.mester@hotmail.com>
2015-01-21 12:54:25 +0100

    get_bookings bugfix

--- test/get_booking.php
+++ test/get_booking.php
@@ -2,11 +2,11 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 include $root . '/model/User.php';
 include $root . '/model/Restaurant.php';
-include $root . '/model/Booking.php';
+
 
 $user = new User(2);
 $rest = new Restaurant(6);
-$booking = new Booking;
-var_dump($booking->get_bookings($user));
+
+var_dump($model->get_bookings($user));
 
 ?>
