d1ea83ffb7873d8e22c2d4709e965295ee276ac9
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-21 13:46:43 +0100

    registreren

--- controller/account.php
+++ controller/account.php
@@ -42,7 +42,7 @@ if($request == '/account/register'){
 	$model = new Model;
 	$attr = $_POST['input'];
 	$bday = $attr["year"].'-'.$attr["month"].'-'.$attr["day"];
-	$attr[$birthdate] = $bday;
+	$attr['birthdate'] = $bday;
 	var_dump($attr);
 	if($model->add_account($attr)){
 		include $root . '/html/index.php';
