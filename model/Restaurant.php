aa8087efd7ddd2319b30dc9b38162dd3ec2ce1e6
Jim <jim.lemmers@gmail.com>
2015-01-21 14:54:34 +0100

    this db check

--- model/Restaurant.php
+++ model/Restaurant.php
@@ -14,16 +14,18 @@ class Restaurant{
 	public $city;
 	public $url;
 	public $tables;
+	private $db;
 	
 
 	function __construct($id){
 		global $db;
+		$this->db = $db;
 
 		$query = "SELECT name, lat, lon," .
 			" street, number, zipcode, city, url" .
 			" FROM restaurant WHERE id = " . $id;
 
-		if($row = get_rows($db->query($query))){
+		if($row = get_rows($this->db->query($query))){
 			foreach($row as $key => $val){
 				$this->$key = $val; 
 			}
