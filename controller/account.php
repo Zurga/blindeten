d7b729c53651d68366d26e233bb998be279a1bfa
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-22 14:01:34 +0100

    forgot password

--- controller/account.php
+++ controller/account.php
@@ -112,6 +112,7 @@ if($request == '/account/forgot_password.php') {
 if($request == '/account/forgot_password'){
 	$email = $_POST['email'];
 	$user = new User(select_id($email));
+	//5 is the 'forgot password' mail in database
 	send_mail($user,5);
 	$new_passw = encrypt($user,$new_passw);
 	$newpquery = "UPDATE user SET password= ".$new_passw." WHERE id= ".$user->id;
