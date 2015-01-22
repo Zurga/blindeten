dcd2ed27550d6400c528900bcf59b16eb2098d72
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-22 16:51:54 +0100

    save password

--- html/admin.php
+++ html/admin.php
@@ -2,40 +2,40 @@
 
 <div class="content">
 	<div class="maincontent">
-		
-			<h1>Administratie</h1>
+	
+		<h1>Administratie</h1>
+		<br>
+		<form action = "change_permission" method="POST">
+			<input id="email" name="email" type="text" placeholder="E-mail" required>   
 			<br>
-			<form action = "change_permission" method="POST">
-					<input id="email" name="email" type="text" placeholder="E-mail" required>   
-					<br>
-					<br>
-					<select name="permission">
-			    	<option value="0">Owner</option>
-			    	<option value="1">Admin</option>
-			    	<option value="2">User</option>
-			    	</select>
-					<br>
-					<br>
-					<br>
-					<input type="submit" id="submit" value="Opslaan">
-			</form>
+			<br>
+			<select name="permission">
+				<option value="0">Owner</option>
+				<option value="1">Admin</option>
+				<option value="2">User</option>
+			</select>
+			<br>
+			<br>
+			<br>
+			<input type="submit" id="submit" value="Opslaan">
+		</form>			
+		
+		<form action="delete_account" method= "POST">
+			<input id="email" name="email" type="text" placeholder="E-mail" required>   
+			<br>
+			<br>
+			<input type="submit" id="submit" value="Account verwijderen">
+		</form>
 				
-				<form action="delete_account" method= "POST">
-					<input id="email" name="email" type="text" placeholder="E-mail" required>   
-					<br>
-					<br>
-					<input type="submit" id="submit" value="Account verwijderen">
-				</form>
+		<form action = "delete_restaurant" method= "POST">
+			<input id="restaurant_name" name="input[restaurant_name]" type="text" placeholder="Naam restaurant" required>	
+			<input type="submit" id="submit" value="Verwijder">
+		</form>
 
-				<form action = "delete_restaurant" method= "POST">
-					<input id="restaurant_name" name="input[restaurant_name]" type="text" placeholder="Naam restaurant" required>	
-					<input type="submit" id="submit" value="Verwijder">
-				</form>
-
-				<form action = "add_restaurant" method= "POST">
-					<input id="restaurant_name" name="input[restaurant_name]" type="text" placeholder="Naam restaurant" required>
-					<input type="submit" id="submit" value="Toevoegen">
-				</form>	
+		<form action = "add_restaurant" method= "POST">
+			<input id="restaurant_name" name="input[restaurant_name]" type="text" placeholder="Naam restaurant" required>
+			<input type="submit" id="submit" value="Toevoegen">
+		</form>	
 	</div>
 </div>
 
