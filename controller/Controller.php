57162ee1cb52d401302d87279de6d1c0ad011151
Jim <jim.lemmers@gmail.com>
2015-01-22 13:53:36 +0100

    bugfix

--- controller/Controller.php
+++ controller/Controller.php
@@ -3,7 +3,7 @@
 include_once 'model/Model.php';
 include_once 'model/Auth.php';
 include_once 'model/User.php';
-include_once 'model/Restaurant';
+include_once 'model/Restaurant.php';
 
 class Controller {
 	public $model;
