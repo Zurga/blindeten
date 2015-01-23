2a5cb37848954f26deebea8905ff42b18c7ad143
Jim <jim.lemmers@gmail.com>
2015-01-23 11:58:22 +0100

    ajax testing

--- js/functions.js
+++ js/functions.js
@@ -46,7 +46,7 @@ function get_output(which, input){
 	http_object = get_http_object();
 	if (http_object != null){
 		http_object.open('POST', "ajax/"+ which, true);
-		http_object.send({'input': input});
+		http_object.send('input=' + input);
 		http_object.onreadystatechange = set_output(input);
 	}
 }
