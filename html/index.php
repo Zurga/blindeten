99fe93f61b5014d5460808734b6d3b100ffcb47b
Jim <jim.lemmers@gmail.com>
2015-01-23 20:36:18 +0100

    ajax test

--- html/index.php
+++ html/index.php
@@ -39,7 +39,8 @@ map = new google.maps.Map(document.getElementById('map'),{
 	center: new google.maps.LatLng(52, 4)
 });
 //create json from the model
-var json = [ <?php foreach($restaurants as $marker){echo json_encode($marker) . ',';}?>];
+var json = { <?php foreach($restaurants as $rest){
+	echo '"' . $rest->id . ':'. json_encode($marker) . ',';}?>};
 
 for(i=0;i<json.length;i++){
 	//create lonlat for each restaurant
