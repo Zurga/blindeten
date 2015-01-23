b8a00f46f735bf6742aa9d7bb1df6ac15d32355e
Jim <jim.lemmers@gmail.com>
2015-01-23 11:55:49 +0100

    ajax testing

--- controller/ajax.php
+++ controller/ajax.php
@@ -1,10 +1,11 @@
 <?php
 
-	echo 'het werkt, het restaurant id = ' . $_GET['input'];
+//	echo 'het werkt, het restaurant id = ' . $_GET['input'];
 	//$bookings = $this->model->get_bookings($_GET['input']);
 	//include $root . '/html/calendar.php';
 
 if($request == '/ajax/booking'){
+	var_dump($_POST);
 	$restaurant = new Restaurant($_POST['input']);
 	if (!isset($_GET['date'])){
 		$bookings = $model->get_bookings($restaurant);
