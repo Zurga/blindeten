8394df6220f8317b8d0960a96316e7a369eead52
Jim <jim.lemmers@gmail.com>
2015-01-26 11:06:45 +0100

    removed var_dumps

--- controller/ajax_controller.php
+++ controller/ajax_controller.php
@@ -23,9 +23,7 @@ if($request == '/ajax/booking'){
 	header('Content-Type: application/json');
 	$input = $_POST['input'];
 	$restaurant = new Restaurant($input['id']);
-	var_dump($input);
 	$bookings = $model->get_bookings($restaurant, $input['date']);
-	var_dump($bookings);
 	if(!empty($bookings)){
 		foreach($bookings as $booking){
 			$user = new User($booking->user1);
@@ -33,7 +31,8 @@ if($request == '/ajax/booking'){
 			$booking->user1['sex'] = ($user->sex == 0 ? 'Man' : 'Vrouw');
 			echo json_encode($bookings);
 		}
-	}
+	}else{
+		echo array();
 }
 
 if($request == 'ajax/book_table'){
