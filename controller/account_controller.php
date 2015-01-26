f134ee4d059598bca5d9d13b4057994625a045e3
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-26 12:21:54 +0100

    email bij welcome

--- controller/account_controller.php
+++ controller/account_controller.php
@@ -46,7 +46,7 @@ if($request == '/account/register'){
 	$attr = $_POST['input'];
 	$bday= $attr['year'].'-'.$attr['month'].'-'.$attr['day'];
 	$attr['birthdate'] = $bday;
-	$email= $_POST['email'];
+	$email= $attr['email'];
 	if($model->add_account($attr)){
 		$query = "SELECT id FROM user WHERE email='". $email."'";
 		if($row = get_rows($this->db->query($query))){
