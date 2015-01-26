c622dc36367782b58fe66d8af607699f328859a9
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-26 11:33:08 +0100

    ingelogd na registreren

--- controller/account_controller.php
+++ controller/account_controller.php
@@ -46,9 +46,8 @@ if($request == '/account/register'){
 	$bday= $attr['year'].'-'.$attr['month'].'-'.$attr['day'];
 	$attr['birthdate'] = $bday;
 	if($model->add_account($attr)){
-		($auth->login($_POST['email'], $_POST['password'])) == True;
-			if($auth->login($_POST['email'], $_POST['password'])){
-			header("Location: ". $index);
+		if($auth->login($attr['email'], $attr['password'])){
+				header("Location: ". $index);
 			}
 			else{
 				include $root . '/html/register.php';
