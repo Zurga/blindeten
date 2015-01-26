1fbb419315e559db094fbf9ced2b2e580928d32f
Jim <jim.lemmers@gmail.com>
2015-01-26 12:20:51 +0100

    booking ajax returns html

--- controller/ajax_controller.php
+++ controller/ajax_controller.php
@@ -1,9 +1,4 @@
 <?php
-
-//	echo 'het werkt, het restaurant id = ' . $_GET['input'];
-	//$bookings = $this->model->get_bookings($_GET['input']);
-	//include $root . '/html/calendar.php';
-
 if($request == '/ajax/calendar'){
 	$input = $_POST['input'];
 	$restaurant = new Restaurant($input['id']);
@@ -26,17 +21,29 @@ if($request == '/ajax/booking'){
 	$bookings = $model->get_bookings($restaurant, $input['date']);
 
 	//check if we have something to return
+	$html = '';
+	$times = array();
 	if(!empty($bookings)){
 		foreach($bookings as $booking){
 			$user = new User($booking->user1);
 			//$booking->user1['age'] = $user->age();
-			$booking->user1['sex'] = ($user->sex == 0 ? 'Man' : 'Vrouw');
-			echo json_encode($bookings);
+			//check the sex of the user
+			$booking->user1 = $user;
+			$html .= '<li id="' . $booking->id . '"><form action="/ajax/book_table" method="POST">' .
+				. $booking->user1->age() . ' ' . $($user->sex == 0 ? 'Man' : 'Vrouw') . 
+				'<input type="field" name="input[rest_id]" value="' .$booking->table_id .'" class="hidden">'.
+				'<input type="submit" value="Reserveer">Reserveer</input></form></li>';
+			$times[$booking->time] += 1;
 		}
-	}else{
-		//return empty array
-		echo json_encode(array());
 	}
+	$html .= '<form action="/ajax/book_table" method="POST"><select name=input[time]>'; 
+	foreach($times as $time){
+		if($times[$time] < count($restaurant->tables)){
+			$html .= '<option value=' . $time . '>' . substring($booking->time, 0, 5) . '</option>';
+		}
+	}
+	$html .= '</select></form>';
+	echo $html;
 }
 
 if($request == 'ajax/book_table'){
