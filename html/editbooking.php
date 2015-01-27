fab5e16cb12f3d0826cb2bafe35a2e30edc9e673
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-27 15:27:43 +0100

    edit booking

--- html/editbooking.php
+++ html/editbooking.php
@@ -8,7 +8,9 @@
 		<form>	
 		<fieldset id="inputs">
 			<li><p>Datum en tijd:  </p>
-			<input id="date" name="input[date]" type="date" placeholder=" <?php echo $booking->date,' ', substr($booking->time, 0, 5); ?>"></p></li>
+			<input id="date" name="input[date]" type="date" placeholder=" <?php echo $booking->date ?>"></li>
+			<br>
+			<input id="time" name="input[time]" type="time" placeholder=" <?php echo substr($booking->time, 0, 5); ?>"></li>
 			<br>
 			<br>
 			<li><input type="submit" id="submit" value="Gegevens opslaan"></p></li>
@@ -16,5 +18,14 @@
 		</form>
     </div>
 </div>
+
+<script>
+	var data = <?php echo json_encode($booking);?>;
+
+		document.getElementById("date").value = data["date"];
+
+		document.getElementById("time").value = data["time"];
+	
+</script>
 
 <?php include 'footer.php';?>
