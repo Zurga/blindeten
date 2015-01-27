cc97c6464b711cac93a03eabbbdfae2a9d223486
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-27 15:37:32 +0100

    edit booking

--- html/editbooking.php
+++ html/editbooking.php
@@ -8,10 +8,10 @@
 		<form>	
 		<fieldset id="inputs">
 			<li><p>Datum: </p>
-			<p><input id="date" name="input[date]" type="date" placeholder="<?php echo $booking->date; ?>"> </p></li>
+			<p><input id="date" name="input[date]" type="date"> </p></li>
 			<br>
 			<li><p>Tijd: </p>
-			<p><input id="time" name="input[time]" type="time" placeholder="<?php echo substr($booking->time, 0, 5); ?>"></p></li>
+			<p><input id="time" name="input[time]" type="time"></p></li>
 			<br>
 			<br>
 			<li><input type="submit" id="submit" value="Gegevens opslaan"></p></li>
