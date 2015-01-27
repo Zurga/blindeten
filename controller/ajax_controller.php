3db344abd1d5219511f182df3e8475ef82ea24c0
Jim <jim.lemmers@gmail.com>
2015-01-27 11:58:30 +0100

    bugfix

--- controller/ajax_controller.php
+++ controller/ajax_controller.php
@@ -49,7 +49,7 @@ if($request == '/ajax/booking'){
 			<option value="20:00">20:00</option>';
 	}
 	$html .= '</select><button value="Reserveer" onClick="'.
-		"var params = 'input[time]='+ document.getElementsById('new-".$restaurant->id."').value;".
+		"var params = 'input[time]='+ document.getElementById('new-".$restaurant->id."').value;".
 		"get_output('book_table',". $restaurant->id .' ,params);">Reserveer</button>';
 	echo $html;
 }
