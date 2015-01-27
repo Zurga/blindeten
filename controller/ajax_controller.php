7b0d794fe339654ac92b1bf8520580b087985f97
Jim <jim.lemmers@gmail.com>
2015-01-27 14:18:57 +0100

    nearly finished

--- controller/ajax_controller.php
+++ controller/ajax_controller.php
@@ -61,6 +61,7 @@ if($request == '/ajax/book_table'){
 		$time = $input['time'];
 		$date = $input['date'];
 		$restaurant = new Restaurant($input['id']);
+		var_dump($restaurant);
 		if(isset($input['booking'])){
 			$booking = new Booking($input['booking']);
 			if($booking = $model->book_table($user, $restaurant, $booking->table, $time)){
