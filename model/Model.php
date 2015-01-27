2a5d3a6a8706f304a42d80a33bdf8c0a4a1e2749
Jim <jim.lemmers@gmail.com>
2015-01-27 17:55:00 +0100

    bugfix

--- model/Model.php
+++ model/Model.php
@@ -128,10 +128,10 @@ class Model{
 		}
 		else if (get_class($object) == 'Restaurant') {
 			$query = "SELECT id FROM bookings WHERE restaurant_id = ". $object->id;
+		}
 			if(isset($user2)){
 				$query .= ' AND user2 IS NULL';
-			
-		}
+			}
 		else {
 			return false;
 		}
