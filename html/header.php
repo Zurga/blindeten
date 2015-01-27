53efde7fc07404a6a78603d26123352915fb0f0f
Fleurkoentjes <fleurkoentjes@gmail.com>
2015-01-27 15:40:02 +0100

    header

--- html/header.php
+++ html/header.php
@@ -11,8 +11,12 @@
 
 <body>
 <div id="header">
-	<a href="/"><img src="/html/blindetenlogo.png"></a>
-<h1> HOI </h1>
+
+	<div class= "headercontent">
+		<a href="/"><img src="/html/blindetenlogo.png"></a>
+		<h1> HOI </h1>
+	</div>
+
 </div>
 
 <div id="menu">
