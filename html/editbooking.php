e4c69cb791ba3cdc927b9a08fe9a16a046682c9c
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-27 15:57:22 +0100

    edit booking

--- html/editbooking.php
+++ html/editbooking.php
@@ -11,7 +11,10 @@
 			<p><input id="date" name="input[date]" type="date"> </p></li>
 			<br>
 			<li><p>Tijd: </p>
-			<p><input id="time" name="input[time]" type="time"></p></li>
+			<select id="time" name="input[time]">
+				<option value="18:00:00">18:00</option>
+				<option value="20:00:00">20:00</option>
+			</select>
 			<br>
 			<br>
 			<li><input type="submit" id="submit" value="Gegevens opslaan"></p></li>
