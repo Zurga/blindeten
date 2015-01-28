052ef61783d0b5f05a37fcb5d4aabda3a01220e7
lauwalittlepony <laura_langeveld@hotmail.com>
2015-01-28 15:47:43 +0100

    delete booking bug fix

--- controller/account_controller.php
+++ controller/account_controller.php
@@ -168,7 +168,7 @@ if($request == '/account/delete_booking') {
 	$booking->other_user($user);
 	if($user->cancel_booking($booking_id)){
 		//mail_id 6 is cancelling booking
-		header("Location: ". $index .'/account/mybookings.php');
+		//header("Location: ". $index .'/account/mybookings.php');
 	}
 }
 ?>
