0999c0358e5864f027cbdd5211be806c62742724
Fleurkoentjes <fleurkoentjes@gmail.com>
2015-01-28 16:04:26 +0100

    mybookings

--- html/mybookings.php
+++ html/mybookings.php
@@ -11,22 +11,22 @@
 			else {
 		
 		foreach($bookings as $booking) { ?>	
-		<p><b>Datum en Tijd: </b><?php echo $booking->date,' ', substr($booking->time, 0, 5); ?></p>
-		
+		<p><b>Datum: </b><?php echo $booking->date?></p>
+		<p><b>Tijd: </b><?php echo substr($booking->time, 0, 5); ?></p>
 		<p><b>Met: </b><?php if($booking->user1->age() == 0 or $booking->user2->age() == 0) {
 		echo 'Er is nog niemand aangeschoven.';
 		} 
 
 		else {if($booking->user2->id == $user->id){ 
 		  if ($booking->user1->sex == 0) {
-		echo 'Man ';
-		} else {echo 'Vrouw ';} echo $booking->user1->age(). ' jaar'; 
+		echo 'Man, ';
+		} else {echo 'Vrouw, ';} echo $booking->user1->age(). ' jaar'; 
 		}
 		
 		else if($booking->user1->id == $user->id){ 
 		  if ($booking->user2->sex == 0) {
-		echo 'Man ';
-		} else {echo 'Vrouw ';} echo $booking->user2->age().' jaar'; 
+		echo 'Man, ';
+		} else {echo 'Vrouw, ';} echo $booking->user2->age().' jaar'; 
 		}} ?> </p>
 		
 		<p><b>Restaurant: </b> <?php $restaurant = new restaurant($booking->restaurant_id); echo $restaurant->name; ?></p>
