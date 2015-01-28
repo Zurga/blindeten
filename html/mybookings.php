1239ea1d11dbed9dd4de9e264e0aacf19c75d11a
Fleurkoentjes <fleurkoentjes@gmail.com>
2015-01-28 17:32:09 +0100

    button

--- html/mybookings.php
+++ html/mybookings.php
@@ -11,6 +11,7 @@
 			else {
 		
 		foreach($bookings as $booking) { ?>	
+		<hr>
 		<p><b>Datum: </b><?php echo $booking->date?></p>
 		<p><b>Tijd: </b><?php echo substr($booking->time, 0, 5); ?></p>
 		<p><b>Met: </b><?php if($booking->user1->age() == 0 or $booking->user2->age() == 0) {
@@ -40,7 +41,7 @@
 		<form action="/account/delete_booking" method="post">
 		<input name="booking_id" value=<?php echo $booking->id; ?> class="hidden">
 	    <input type="submit" id="submit" value="Reservering verwijderen" style="float:right">
-	    <br><hr>
+	    <br>
 		</form>
 		
 
