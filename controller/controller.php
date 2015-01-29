a9132fe42740e952c99bce272d11526f5de9085a
Jim <jim.lemmers@gmail.com>
2015-01-29 17:28:08 +0100

    bugfix

--- controller/controller.php
+++ controller/controller.php
@@ -17,7 +17,6 @@ class Controller {
 		$index = 'http://' . $_SERVER['SERVER_NAME'];
 		$root = $_SERVER['DOCUMENT_ROOT'];
 		$request = $_SERVER['REQUEST_URI'];
-		echo $request;
 
 		//get user info if logged in
 		if($logged_in){
