e3d0cc57b30dbc3559b9886450abb22a4d191de0
Jim <jim.lemmers@gmail.com>
2015-01-30 14:46:23 +0100

    css

--- html/info.php
+++ html/info.php
@@ -5,8 +5,8 @@
 		<p>Zin om uit eten te gaan? Maar u bent alleen en wilt nieuwe mensen ontmoeten? Of gewoon gezellig met iemand uit eten gaan? Dan bent u bij ons aan het goede adres!</p>
 		<h2>Hoe werkt het?</h2>
 		<p>Heel simpel! Op het kaartje ziet u de verschillende restaurants staan waar u kunt reserveren. Bent u de eerste die ergens reserveert, dan moet u nog even geduld hebben. Wanneer er een persoon bij u aan tafel schuift krijgen jullie beiden een bevestigingsmail en wensen wij jullie een gezellig diner toe!</p>
-		<h2>Als u uw restaurant wilt aanmelden, neem dan <a href="/html/contact.php">contact</a> met ons op.</h2>	
-		<h2>Voor vragen zie de <a href="faq.php" title="Frequently Asked Questions">FAQ</a></h2>
+		<p>Als u uw restaurant wilt aanmelden, neem dan <a href="/html/contact.php">contact</a> met ons op.</p>	
+		<p>Voor vragen zie de <a href="faq.php" title="Frequently Asked Questions">FAQ</a><p>
 	</div>
 </div>
 <?php include 'footer.php';?>
