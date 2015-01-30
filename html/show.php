2d20ce151826da728a1a396b55e5aa6977121398
Jim <jim.lemmers@gmail.com>
2015-01-30 13:34:22 +0100

    removed brs in show

--- html/show.php
+++ html/show.php
@@ -3,7 +3,7 @@
 	<div class="content">
 	<div class="maincontent">
 		<h1>Mijn Account</h1>
-		<br><?php
+		<?php
 			if(isset($changed_password)) {
 				echo '<p class="confirm">'. $changed_password .'</p>';
 			}
@@ -13,16 +13,12 @@
 			if(isset($changed_restaurant)) {
 				echo '<p class="confirm">'. $changed_restaurant .'</p>';
 			}?>
-		<br>
+		
 		<p><b>Naam: </b> <?php echo $user->name ,' ', $user->surname; ?></p>
 		<p><b>Geslacht: </b> <?php if ($user->sex == 0) {echo 'Man';} else {echo 'Vrouw';} ?></p>
 		<p><b>Geboortedatum: </b> <?php echo $user->birthdate; ?></p>
 		<p><b>Woonplaats: </b> <?php echo $user->city; ?></p>
-		<br>
-		<br>
 		<p><a href="/account/edit.php" title="Gegevens wijzigen">Gegevens wijzigen</a>
-		<br>		
-		<br>
 		<a href="/account/change_password.php" title="Wachtwoord veranderen">Wachtwoord veranderen</a>
 		</p>		
 	</div>
@@ -36,9 +32,8 @@ echo '
 		<p><b>Naam: </b> '. $restaurant->name .'</p>
 		<p><b>URL: </b><a href="'.$restaurant->url .'">'.$restaurant->url.'</a></p>
 		<p><b>Aantal tafels: </b>'.count($restaurant->tables).'</p>
-		<p><b>Adres: </b>'. $restaurant->street.' '.$restaurant->number.'<br>'. $restaurant->zipcode.' '. $restaurant->city.'<br><br></p>	
-		<p><a href="/account/edit_restaurant.php" title="Gegevens wijzigen">Gegevens wijzigen</a>
-		</p>	
+		<p class="adres"><b>Adres: </b>'. $restaurant->street.' '. $restaurant->number .' ' .$restaurant->zipcode.'</p><p> '. $restaurant->city.'</p>	
+		<p><a href="/account/edit_restaurant.php" title="Gegevens wijzigen">Gegevens wijzigen</a></p>	
 	</div>
 </div>';
 } ?>
