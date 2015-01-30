01adc6c7a688107c5c97ee28df306396e6df56ea
Fleurkoentjes <fleurkoentjes@gmail.com>
2015-01-30 12:36:25 +0100

    admin

--- html/admin.php
+++ html/admin.php
@@ -9,25 +9,26 @@
 		<br>
 		<div id="Tabs">
 			<ul>
-				<li id="tab_one" OnClick="tab('tab1')"><a>Verwijderen</a></li> 
-				<li id="tab_two" OnClick="tab('tab2')"><a>Toevoegen</a></li>
-				<li id="tab_three" OnClick="tab('tab3')"><a>Permissie</a></li>
+				<li id="tab_one" OnClick="tab('tab1')"><a>Gebruiker</a></li> 
+				<li id="tab_two" OnClick="tab('tab2')"><a>Restaurants</a></li>
 			</ul>
 	
 			<div id="tab_content">
 			
 				<div id="tab1">
-					<b><h2>Verwijder restaurant</h2></b>
-					<form action = "delete_restaurant" method= "POST" id="admin_form">
-					<select name="rest_id">
-						<?php 
-						foreach($restaurants as $restaurant){
-						echo '<option value ='.$restaurant->id.'>'.$restaurant->name.'</option>';
-						} ?>
+					<b><h2>Verander permissie van gebruiker:</h2></b>
+					<form action = "change_permission" method="POST" id= "admin_form">
+					<input id="email" name="email" type="text" placeholder="E-mail" required>   
+					<br>
+					<select name="permission">
+						<option value="0">Owner</option>
+						<option value="1">Admin</option>
+						<option value="2">User</option>
 					</select>
 					<br><br>
-					<input type="submit" id="submit" value="Verwijder">
-					</form>
+					<input type="submit" id="submit" value="Opslaan">
+					</form>	
+
 					<b><h2>Verwijderen van account:</h2></b>
 					<form action="delete_account" method= "POST" id="admin_form">
 					<input id="email" name="email" type="text" placeholder="E-mail" required>   
@@ -37,6 +38,7 @@
 				</div>
 			
 				<div id="tab2" style="display:none">
+					
 					<b><h2>Toevoegen restaurant:</h2></b>
 					<form action = "add_restaurant" method= "POST" id="admin_form">
 					<input id="name" name='input["name"]' type="text" placeholder="Naam restaurant" required>
@@ -50,22 +52,20 @@
 					<br><br>
 					<input type="submit" id="submit" value="Toevoegen">
 					</form>	
-				</div>
-		
-				<div id="tab3" style="display:none">
-					<b><h2>Verander permissie van gebruiker:</h2></b>
-					<form action = "change_permission" method="POST" id= "admin_form">
-					<input id="email" name="email" type="text" placeholder="E-mail" required>   
-					<br>
-					<select name="permission">
-						<option value="0">Owner</option>
-						<option value="1">Admin</option>
-						<option value="2">User</option>
+
+					<b><h2>Verwijder restaurant</h2></b>
+					<form action = "delete_restaurant" method= "POST" id="admin_form">
+					<select name="rest_id">
+						<?php 
+						foreach($restaurants as $restaurant){
+						echo '<option value ='.$restaurant->id.'>'.$restaurant->name.'</option>';
+						} ?>
 					</select>
 					<br><br>
-					<input type="submit" id="submit" value="Opslaan">
-					</form>	
-				</div>	
+					<input type="submit" id="submit" value="Verwijder">
+					</form>
+				</div>
+		
 			
 
 			</div>
@@ -78,10 +78,8 @@
 function tab(tab) {
 document.getElementById('tab1').style.display = 'none';
 document.getElementById('tab2').style.display = 'none';
-document.getElementById('tab3').style.display = 'none';
 document.getElementById('tab_one').setAttribute("class", "");
 document.getElementById('tab_two').setAttribute("class", "");
-document.getElementById('tab.three').setAttribute("class","");
 document.getElementById(tab).style.display = 'block';
 document.getElementById('tab_'+tab).setAttribute("class", "active");
 }
